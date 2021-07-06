11b88de79d24f153516579560ca0bf6f37f8fa9b
Shankar11 <sshankar11@gmail.com>
2021-07-06 12:27:56 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component, VERSION } from '@angular/core';
 
+function log(target, name, descriptor) {
+  console.log(target, name, descriptor);
+}
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
-  
+
+  @log
+  aSimpleMethod() {
+    console.log('Hey there');
+  }
 }
